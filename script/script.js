697ab197f6eca36b217d48e5b7946e8567dd9caa
Joel Lassiter <jlassiter@ingage.io>
2023-12-23 10:59:33 -0500

    Update portfolio item text; add .gitignore

--- script/script.js
+++ script/script.js
@@ -1,15 +1,15 @@
 const navLinks = document.querySelectorAll('.nav-item');
 const menuToggle = document.getElementById('navbarSupportedContent');
 
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() });
-})
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() });
+});
 
 const yearsActiveEl = document.getElementById('years-active');
 
-const startDate = new Date('2004-10-01');
+const startDate = new Date('2004-08-01');
 var diff = (new Date().getTime() - startDate.getTime()) / 1000;
 diff /= (60 * 60 * 24);
 
 const yearsActiveCalc = Math.abs(Math.round(diff/365.25));
-yearsActiveEl.innerHTML = `<b>Years Active:</b> ${yearsActiveCalc}`
+yearsActiveEl.innerHTML = `<b>Years Active:</b> ${yearsActiveCalc}`;
